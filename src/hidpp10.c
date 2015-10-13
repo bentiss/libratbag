b9a51a516b939b1b714a1a610803bf570d044741
Peter Hutterer <peter.hutterer@who-t.net>
2015-10-14 08:36:32 +1000

    hidpp10: convert 0x00 Enable HID++ Reporting into a proper bitmask
    The caller doesn't need to worry about registers or something here, let's just
    expose one single bitmask, provided by an enum.
    This also adds s couple of missing bits from the previous implementation.
    
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

--- src/hidpp10.c
+++ src/hidpp10.c
@@ -211,8 +211,7 @@ out_err:
 
 int
 hidpp10_get_hidpp_notifications(struct hidpp10_device *dev,
-				uint8_t *reporting_flags_r0,
-				uint8_t *reporting_flags_r2)
+				uint32_t *reporting_flags)
 {
 	unsigned idx = dev->index;
 	union hidpp10_message notifications = CMD_HIDPP_NOTIFICATIONS(idx, GET_REGISTER_REQ);
@@ -224,16 +223,16 @@ hidpp10_get_hidpp_notifications(struct hidpp10_device *dev,
 	if (res)
 		return res;
 
-	*reporting_flags_r0 = notifications.msg.parameters[0];
-	*reporting_flags_r2 = notifications.msg.parameters[2];
+	*reporting_flags = notifications.msg.parameters[0];
+	*reporting_flags |= (notifications.msg.parameters[0] & 0x1F) << 8;
+	*reporting_flags |= (notifications.msg.parameters[2] & 0x7 ) << 16;
 
 	return res;
 }
 
 int
 hidpp10_set_hidpp_notifications(struct hidpp10_device *dev,
-				uint8_t reporting_flags_r0,
-				uint8_t reporting_flags_r2)
+				uint32_t reporting_flags)
 {
 	unsigned idx = dev->index;
 	union hidpp10_message notifications = CMD_HIDPP_NOTIFICATIONS(idx, SET_REGISTER_REQ);
@@ -241,8 +240,9 @@ hidpp10_set_hidpp_notifications(struct hidpp10_device *dev,
 
 	hidpp_log_raw(&dev->base, "Setting HID++ notifications\n");
 
-	notifications.msg.parameters[0] = reporting_flags_r0;
-	notifications.msg.parameters[2] = reporting_flags_r2;
+	notifications.msg.parameters[0] = reporting_flags & 0xFF;
+	notifications.msg.parameters[1] = (reporting_flags >> 8) & 0x1F;
+	notifications.msg.parameters[2] = (reporting_flags >> 16) & 0x7;
 
 	res = hidpp10_request_command(dev, &notifications);
 
@@ -1189,8 +1189,7 @@ hidpp10_get_firmare_information(struct hidpp10_device *dev,
 static int
 hidpp10_get_device_info(struct hidpp10_device *dev)
 {
-	uint32_t feature_mask;
-	uint8_t f1, f2;
+	uint32_t feature_mask, notifications;
 	uint8_t reflect;
 	int i;
 	uint16_t xres, yres;
@@ -1200,7 +1199,7 @@ hidpp10_get_device_info(struct hidpp10_device *dev)
 	struct hidpp10_profile profiles[HIDPP10_NUM_PROFILES];
 
 	hidpp10_get_individual_features(dev, &feature_mask);
-	hidpp10_get_hidpp_notifications(dev, &f1, &f2);
+	hidpp10_get_hidpp_notifications(dev, &notifications);
 
 	hidpp10_get_current_resolution(dev, &xres, &yres);
 	hidpp10_get_led_status(dev, led);
