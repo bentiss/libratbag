4981c772928ae62ab325504b2fabac688872abfd
Peter Hutterer <peter.hutterer@who-t.net>
2015-10-15 12:38:11 +1000

    hidpp10: drop a couple of unused fields from the hidpp10 device
    Some were truly unused, others were write-only.
    
    Signed-off-by: Peter Hutterer <peter.hutterer@who-t.net>

--- src/hidpp10.c
+++ src/hidpp10.c
@@ -1201,20 +1201,25 @@ hidpp10_get_device_info(struct hidpp10_device *dev)
 	uint8_t f1, f2;
 	uint8_t reflect;
 	int i;
+	uint16_t xres, yres;
+	uint16_t refresh_rate;
+	enum hidpp10_led_status led[6];
+	int8_t current_profile;
+	struct hidpp10_profile profiles[HIDPP10_NUM_PROFILES];
 
 	hidpp10_get_individual_features(dev, &f1, &f2);
 	hidpp10_get_hidpp_notifications(dev, &f1, &f2);
 
-	hidpp10_get_current_resolution(dev, &dev->xres, &dev->yres);
-	hidpp10_get_led_status(dev, dev->led);
-	hidpp10_get_usb_refresh_rate(dev, &dev->refresh_rate);
+	hidpp10_get_current_resolution(dev, &xres, &yres);
+	hidpp10_get_led_status(dev, led);
+	hidpp10_get_usb_refresh_rate(dev, &refresh_rate);
 
 	hidpp10_get_optical_sensor_settings(dev, &reflect);
 
-	hidpp10_get_current_profile(dev, &dev->current_profile);
+	hidpp10_get_current_profile(dev, &current_profile);
 
 	for (i = 0; i < HIDPP10_NUM_PROFILES; i++)
-		hidpp10_get_profile(dev, i, &dev->profiles[i]);
+		hidpp10_get_profile(dev, i, &profiles[i]);
 
 	return 0;
 }
